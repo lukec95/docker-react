16884ebf3159bc1d8c0fb5624495d25d77cb48cd
Luke Chowles <luke.chowles@ibm.com>
2021-10-13 13:14:19 +0100

    my new feature d

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. CHANGES MADE A 5th TIME
+          Edit <code>src/App.js</code> and save to reload. CHANGES MADE A 6th TIME
         </p>
         <a
           className="App-link"
